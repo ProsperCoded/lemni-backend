207406fb1c5d416a9cafb1a714f6e9f30ce9089d
prospercoded <enweremproper@gmail.com>
2026-07-04 14:14:33 +0100

    fix

--- src/billing/billing.module.ts
+++ src/billing/billing.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { BillingService } from './billing.service';
 import { ProrationService } from './proration.service';
 import { BillingController } from './billing.controller';
+import { CheckoutModule } from '../checkout/checkout.module';
 
 @Module({
+  imports: [CheckoutModule],
   controllers: [BillingController],
   providers: [BillingService, ProrationService],
   exports: [BillingService, ProrationService],
