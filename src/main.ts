301fdc296c88b62a484dca5837db1fbf2c4354fe
prospercoded <enweremproper@gmail.com>
2026-07-04 13:46:28 +0100

    Enable Swagger docs in all environments

--- src/main.ts
+++ src/main.ts
@@ -5,24 +5,22 @@ import { AppModule } from './app.module';
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
-  if (process.env.NODE_ENV !== 'production') {
-    const config = new DocumentBuilder()
-      .setTitle('LEMNI PaaS Core Engine API')
-      .setDescription(
-        'Deterministic, highly resilient backend infrastructure for the Lemni Payment-as-a-Service (PaaS) engine.',
-      )
-      .setVersion('1.0.0')
-      .addBearerAuth()
-      .build();
+  const config = new DocumentBuilder()
+    .setTitle('LEMNI PaaS Core Engine API')
+    .setDescription(
+      'Deterministic, highly resilient backend infrastructure for the Lemni Payment-as-a-Service (PaaS) engine.',
+    )
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
 
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('docs', app, document, {
-      swaggerOptions: {
-        persistAuthorization: true,
-        tagsSorter: 'alpha',
-      },
-    });
-  }
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+      tagsSorter: 'alpha',
+    },
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
